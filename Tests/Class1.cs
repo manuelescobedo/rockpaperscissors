ae55de00a0bf19973409be8f71fd14fdf31a19c8
Mani <bigmander@gmail.com>
2022-01-09 14:03:48 -0600

    Remove tests

---
+++ Tests/Class1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Class1
+    {
+
+        [TestMethod]
+        public void Foo()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
